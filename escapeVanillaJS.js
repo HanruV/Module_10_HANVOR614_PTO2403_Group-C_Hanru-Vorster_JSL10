6332c7773239cf3a1fe8332940ca99ced5c98527
Hanru Vorster <hanru.vorster@gmail.com>
2024-09-12 09:51:32 +0200

    Fixed the findMostRecentBook function to display the most recently published book

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // 🪲 Bug: Incorrect ID used for attaching the event listener
+  // 🪲 Bug: Incorrect ID used for attaching the event listener - FIXED
   document.getElementById("solveRoom1").addEventListener("click", () => {
     fetch("books.json")
       .then((response) => response.json())
       .then((books) => {
         const mostRecentBook = findMostRecentBook(books);
-        // 🪲 Bug: Incorrect element ID
-        document.getElementById(
-          "room1Result"
-        ).textContent = `The key to the next room is: ${mostRecentBook.title}`;
+        // 🪲 Bug: Incorrect element ID - FIXED
+        document.getElementById("room1Result").textContent =
+          mostRecentBook.title;
       });
   });
 
@@ -39,12 +38,11 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function findMostRecentBook(books) {
-  // 🪲 Bug: Logic error
-  return books.reduce((mostRecent, book) =>
-    new Date(book.published) < new Date(mostRecent.published)
-      ? book
-      : mostRecent
-  );
+  return books.reduce((first, current) => {
+    return new Date(current.published) > new Date(first.published)
+      ? current
+      : first;
+  });
 }
 
 function findIntersection(setA, setB) {
